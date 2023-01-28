e82e27192e6d2e2317f02349ed8fe6902983a3af
kushgarg27 <76466199+kushgarg27@users.noreply.github.com>
2023-01-28 20:18:08 +0530

    Basic Intial UI

---
+++ app/src/main/java/com/kush/notesmvvm/Add_Note.kt
@@ -0,0 +1,11 @@
+package com.kush.notesmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Add_Note : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+    }
+}
